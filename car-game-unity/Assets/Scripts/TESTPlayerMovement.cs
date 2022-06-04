162ad3e6628c671fc11f5c6a084e402cf6961e0d
Andrew Finsand <andfinsand@gmail.com>
2022-06-04 12:19:29 -0700

    First commit.

---
+++ car-game-unity/Assets/Scripts/TESTPlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float moveSpeed;
+    private bool isTurning;
+    public float turnSpeed;
+   
+
+    // Start is called before the first frame update
+   
+    void Update()
+    {
+        // This is movement code
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+        // this is turning
+        if (Input.GetMouseButtonDown(1))
+        {
+            isTurning = true;
+        }
+        if (isTurning == true)
+        {
+            float rot = Input.GetAxis("Mouse X") * turnSpeed;
+            transform.Rotate(Vector3.up, rot * Time.deltaTime);
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            isTurning = false;
+        }
+    }
+}
---
+++ car-game-unity/Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform Porsche;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Porsche.position + offset;
+    }
+}
